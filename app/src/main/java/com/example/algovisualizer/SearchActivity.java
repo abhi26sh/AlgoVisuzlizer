b3ff5ac1e4ba1154db7eccc2142b9eb4023e77c3
abhi26sh <abhi.001shuklg@gmail.com>
2021-09-07 15:54:24 +0530

    name changed gridview added searching first page added tried to implement seekbar not working!!!!!!!!!!

---
+++ app/src/main/java/com/example/algovisualizer/SearchActivity.java
@@ -0,0 +1,42 @@
+package com.example.algovisualizer;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.sdsmdg.harjot.crollerTest.Croller;
+import com.sdsmdg.harjot.crollerTest.OnCrollerChangeListener;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
+
+
+public class SearchActivity extends AppCompatActivity {
+    //@Override
+   // TextView ArraySize=findViewById(R.id.showArraySizeTV);
+   // String sizeOfArray="0";
+    /*protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        Croller croller = (Croller) findViewById(R.id.croller);
+
+        croller.setIndicatorWidth(10);
+        croller.setBackCircleColor(Color.parseColor("#EDEDED"));
+        croller.setMainCircleColor(Color.WHITE);
+        croller.setMax(50);
+        croller.setStartOffset(45);
+        croller.setIsContinuous(false);
+        croller.setLabelColor(Color.BLACK);
+        croller.setProgressPrimaryColor(Color.parseColor("#0B3C49"));
+        croller.setIndicatorColor(Color.parseColor("#0B3C49"));
+        croller.setProgressSecondaryColor(Color.parseColor("#EEEEEE"));
+        croller.setOnProgressChangedListener(new Croller.onProgressChangedListener() {
+            @Override
+            public void onProgressChanged(Croller croller,int progress) {
+                // use the progress
+                 sizeOfArray =progress.toString();
+
+            }
+        });*/
+  //  }
+}
